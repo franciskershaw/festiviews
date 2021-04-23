8f47a0ad5e7ecd3eed8d30253e57fc3df5e069fd
franciskershaw <franciskershaw2004@hotmail.com>
2021-04-23 10:05:02 +0000

    add: bootstrap navbar to base.html, link index

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(
         host=os.environ.get('IP', '0.0.0.0'),
         port=int(os.environ.get('PORT', '5000')),
+        # DON'T FORGET TO CHANGE THIS TO FALSE BEFORE SUBMISSION
         debug=True
     )
