53ba260229042622088add2245a87e3e6261c497
franciskershaw <franciskershaw2004@hotmail.com>
2021-05-05 10:28:47 +0000

    fix: js script not loading on internal pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
-
-$('.navbar-toggler').click(function(){
-    $('#navbar-open-icon').toggleClass('hide')
-    $('#navbar-close-icon').toggleClass('hide')
+document.addEventListener("DOMContentLoaded", function (event) {
+    $('.navbar-toggler').click(function () {
+        $('#navbar-open-icon').toggleClass('hide')
+        $('#navbar-close-icon').toggleClass('hide')
+    });
 })
