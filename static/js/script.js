c4a464e286c79c4abc4b252c831f0db74c27b25e
franciskershaw <franciskershaw2004@hotmail.com>
2021-05-07 11:47:50 +0000

    autocomplete edit_festival fields using javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,59 @@
+// Existing pages
+const pages = {
+    add_festival: false,
+    add_review: false,
+    browse: false,
+    edit_festival: false,
+    edit_review: false,
+    faq: false,
+    favourites: false,
+    index: false,
+    login: false,
+    signup: false,
+    view_festival: false
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
+    // Alternates between three bars and 'x' on mobile navbar
     $('.navbar-toggler').click(function () {
         $('#navbar-open-icon').toggleClass('hide')
         $('#navbar-close-icon').toggleClass('hide')
     });
+
+    assignPageTrue();
+    console.log(pages);
+    if(pages.edit_festival) {
+        populateEditForm();
+    }
 })
+
+/*
+This function checks which page the user is on and assigns the relevant variable
+above as 'true', for when certain functions are only needed on certain pages.
+*/
+
+function assignPageTrue() {
+    const keys = Object.keys(pages);
+    const pageSectionClasses = document.querySelector('section').classList;
+    keys.forEach((key) => {
+        for (sectionClass of pageSectionClasses) {
+            if (sectionClass == key) {
+                pages[key] = true;
+            }
+        }
+    })
+}
+
+/*
+This function loops over options in select dropdowns and adds the 'selected'
+attribute to the correct option when editting a form.
+*/
+function populateEditForm() {
+    let options = document.querySelectorAll('option')
+    const selectedGenre = document.querySelector('#selected-genre').innerHTML;
+    for (option of options) {
+        if (option.innerHTML === selectedGenre) {
+            option.setAttribute('selected', 'true')
+        }
+    }
+}
