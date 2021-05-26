b582c0803d524ded6ad328fa143a1749555b5a02
franciskershaw <franciskershaw2004@hotmail.com>
2021-05-26 17:06:34 +0000

    add error pages and route through app.py

--- app.py
+++ app.py
@@ -447,6 +447,16 @@ def page_not_found(error):
     return render_template('errors/404.html'), 404
 
 
+@app.errorhandler(403)
+def error_403():
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(500)
+def error_500():
+    return render_template('errors/500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(
         host=os.environ.get('IP'),
