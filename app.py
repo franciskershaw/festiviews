a6fa6c7a70f12f801800c2ce3cdf5b9dad861e2a
franciskershaw <franciskershaw2004@hotmail.com>
2021-06-01 17:57:01 +0000

    remove print statements from app.py

--- app.py
+++ app.py
@@ -37,37 +37,31 @@ def update_average_rating(festival_id):
         {'_id': ObjectId(festival_id)})
     # Find corresponding reviews
     review_ids = festival_to_update['reviews']
-    print(f'Festival name is: {festival_to_update["name"]}')
-    print('----------------------------------')
-    print(f'Review IDs in an array: {review_ids}')
-    print('----------------------------------')
-    # Loop over reviews and append the ratings to rating_arr
-    for review_id in review_ids:
-        review = mongo.db.reviews.find_one({'_id': review_id})
-        rating_arr.append(int(review['rating']))
-    print(f'Ratings array as integers: {rating_arr}')
-    # Sum the values of the array
-    sum_of_arr = sum(rating_arr)
-    print(f'Sum of the array: {sum_of_arr}')
-    # Get the length of the array
-    len_of_arr = len(rating_arr)
-    print(f'Length of the array: {len_of_arr}')
-    # Work out the average of the array
-    av_rating_float = sum_of_arr / len_of_arr
-    print(f'Average rating as float is {av_rating_float}')
-    print('----------------------------------')
 
-    # Round the average rating to the nearest .5
-    # https://stackoverflow.com/questions/24838629/round-off-float-to-nearest-0-5-in-python
-    rating_rounded = round(av_rating_float * 2)/2
-    print(f'Rating rounded: {rating_rounded}')
-    print('----------------------------------')
-    print('----------------------------------')
+    if len(review_ids) != 0:
+        # Loop over reviews and append the ratings to rating_arr
+        for review_id in review_ids:
+            review = mongo.db.reviews.find_one({'_id': review_id})
+            rating_arr.append(int(review['rating']))
+        # Sum the values of the array
+        sum_of_arr = sum(rating_arr)
+        # Get the length of the array
+        len_of_arr = len(rating_arr)
+        # Work out the average of the array
+        av_rating_float = sum_of_arr / len_of_arr
+        # Round the average rating to the nearest .5
+        # https://stackoverflow.com/questions/24838629/round-off-float-to-nearest-0-5-in-python
+        rating_rounded = round(av_rating_float * 2)/2
 
-    # Add completed average rating to the DB
-    mongo.db.festivals.update_one(
-        {'_id': ObjectId(festival_id)},
-        {"$set": {"average_rating": rating_rounded}})
+        # Add completed average rating to the DB
+        mongo.db.festivals.update_one(
+            {'_id': ObjectId(festival_id)},
+            {"$set": {"average_rating": rating_rounded}})
+
+    else:
+        mongo.db.festivals.update_one(
+            {'_id': ObjectId(festival_id)},
+            {"$unset": {'average_rating': ""}})
 
 
 @app.route('/')
